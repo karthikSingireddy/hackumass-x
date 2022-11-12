073d4d1de3d9da5654c5d371da9754f586ca5063
karthikSingireddy <skarthikreddy189@gmail.com>
2022-11-12 16:40:48 -0500

    centered images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,13 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+// import AdbIcon from '@mui/icons-material/Adb';
 
 const pages = ['worcester', 'frank', 'hamp', 'berk'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -20,7 +20,7 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 function App() {
   const [anchorElNav, setAnchorElNav] = React.useState("worcester");
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const [diningHall, setDiningHall] = React.useState(null);
+  const [diningHall, setDiningHall] = React.useState('worcester');
 
   // const handleOpenNavMenu = () => {
   //   setAnchorElNav(Event.currentTarget);
--- frontend/src/DiningHall.js
+++ frontend/src/DiningHall.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 function DiningHall(props) {
     return (
-        <div className='diningHall' onClick={() => {console.log(props.name)}}>
-            <img src={require("./" + props.name + ".jpg")} alt="image not found"/>
+        <div className='diningHall' onClick={() => {console.log(props.name)}}
+            style={{
+                display: 'flex'
+            }}
+        >
+            <img 
+                src={require("./" + props.name + ".jpg")} 
+                // alt="image not found"
+                style={{
+                    height: '90vh',
+                    margin: 'auto'
+                }}
+            />
         </div>
     );
 }
