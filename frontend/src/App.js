a8bec46c2d6886c4961142c103203cc9d428f059
Jacob Epstein <jacobepstein90@gmail.com>
2022-11-12 02:39:16 -0500

    added state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,15 +19,17 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 function App() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [diningHall, setDiningHall] = React.useState(null);
 
-  const handleOpenNavMenu = () => {
-    setAnchorElNav(Event.currentTarget);
-  };
-  const handleOpenUserMenu = () => {
-    setAnchorElUser(Event.currentTarget);
-  };
+  // const handleOpenNavMenu = () => {
+  //   setAnchorElNav(Event.currentTarget);
+  // };
+  // const handleOpenUserMenu = () => {
+  //   setAnchorElUser(Event.currentTarget);
+  // };
 
   const handleCloseNavMenu = () => {
+    console.log("clicked this");
     setAnchorElNav(null);
   };
 
@@ -39,66 +41,11 @@ function App() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
                 key={page}
-                onClick={handleCloseNavMenu}
+                onClick={() => {setDiningHall(page); console.log("state is " + page)}}
                 sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 {page}
