f31cf2c43867591908561998708ad63e4c1438ca
Jacob Epstein <jacobepstein90@gmail.com>
2022-11-12 23:23:58 -0500

    added time, reformatted

--- frontend/src/DiningHall.js
+++ frontend/src/DiningHall.js
@@ -4,13 +4,15 @@ import './diningHall.css'
 import frank from './frank';
 import hamp from './hamp';
 import woo from './woo';
-
+import Time from 'react-time';
 class DiningHall extends React.Component {
     constructor(props) {
         super(props);
         console.log(props);
         this.props = props;
         this.canvas = React.createRef();
+        // const context = this.canvas.current.getContext('2d');    
+        // context.translate(0.5, 0.5);
         this.state = { t: 0 };
     }
 
@@ -21,13 +23,41 @@ class DiningHall extends React.Component {
             .then(data => {
                 t.refresh();
                 data.tables.forEach(table => {
-                    t.drawTable(table.x, table.y, table.width, table.height, table.taken);
+                    let elapsed = Date.now() - table.time;
+                    let sec = Math.ceil((elapsed / 1000));
+                    let min = Math.ceil((elapsed) / (1000 * 60));
+                    console.log(Date.now());
+                    t.drawTable(table.x, table.y, table.width, table.height, table.taken, min + ":" + sec);
                 })
             });
     }
 
     componentDidMount() {
         this.componentDidUpdate(null, null);
+        //get DPI
+        let dpi = window.devicePixelRatio;
+        //get context
+        let ctx = this.canvas.current.getContext('2d');
+        const originalHeight = this.canvas.current.height;
+        const originalWidth = this.canvas.current.width;
+        console.log("original height is " + originalHeight);
+        //get CSS height
+        //the + prefix casts it to an integer
+        //the slice method gets rid of "px"
+        let style_height = +getComputedStyle(this.canvas.current).getPropertyValue("height").slice(0, -2);
+        //get CSS width
+        let style_width = +getComputedStyle(this.canvas.current).getPropertyValue("width").slice(0, -2);
+        //scale the canvas
+        this.canvas.current.setAttribute('height', style_height * dpi);
+        this.canvas.current.setAttribute('width', style_width * dpi);
+        console.log("new height: " + this.canvas.current.height);
+        let ratio = Math.max(
+            originalWidth/style_width * dpi,
+            originalHeight/style_height * dpi
+        );
+        console.log("ratio is " + ratio);
+        ctx.scale(8, 8);
+        ctx.translate(35, 0);
         this.interval = setInterval(() => this.setState({ t: Date.now()}), 1000);
     }
 
@@ -61,7 +91,7 @@ class DiningHall extends React.Component {
         }
     }
 
-    drawTable(x, y, width, height, taken) {
+    drawTable(x, y, width, height, taken, time) {
         console.log('draw table')
         const canvas = this.canvas.current;
         const ctx = canvas.getContext('2d');
@@ -69,6 +99,12 @@ class DiningHall extends React.Component {
 
         ctx.fillStyle = color;
         ctx.fillRect(x, y, width, height);
+        if (taken) {
+            ctx.fillStyle = '#000000';
+            ctx.font = "4px courier new";
+            ctx.moveTo(x, y);
+            ctx.fillText(time.toString(), x+1.9, y+8.35);
+        }
     }
 
     render() {
