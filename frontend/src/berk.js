ef1b444d958d6c9cd18b826b6339b0b149beb7c4
Jacob Epstein <jacobepstein90@gmail.com>
2022-11-12 21:05:26 -0500

    berk & frank

--- frontend/src/berk.js
+++ frontend/src/berk.js
@@ -1,6 +1,19 @@
 function draw(ctx) {
   ctx.fillStyle = "#000000";
-  ctx.fillRect(0, 0, 20, 30);
+  ctx.fillRect(90,10,60,2);
+  ctx.fillRect(90,10,2,50);
+  ctx.fillRect(82,60,10,2);
+  ctx.fillRect(82,60,2,20);
+  ctx.fillRect(82,78,10,2);
+  ctx.fillRect(90,78,2,30);
+  ctx.fillRect(90,108,15,2);
+  ctx.fillRect(105,108,2,20);
+  ctx.fillRect(105,128,80,2);
+  ctx.fillRect(183,68,2,60);
+  ctx.fillRect(183,68,30,2);
+  ctx.fillRect(150,10,2,35);
+  ctx.fillRect(150,45,63,2);
+  ctx.fillRect(213,45,2,25);
 }
 
 export default {
